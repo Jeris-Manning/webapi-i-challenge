751b843e825911ba56a8ce67643f6c7cddd0080d
Jeris Manning <the.goose.moose@gmail.com>
2019-11-02 14:15:18 -0600

    adds endpoint to retrieve all users in db

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // implement your API here
+
+const express = require('express');
+const db = require('./data/db.js');
+
+const server = express();
+
+server.listen(7777, () => {
+  console.log('Server is listening on port 7777, yo!');
+});
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: err,
+        success: false
+      });
+    });
+});
